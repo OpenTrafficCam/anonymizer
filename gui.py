6d30a6e7656f43fbf2ae5a29ebbba5e472518767
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2021-11-12 13:58:02 +0100

    Create placeholder py for gui

---
+++ gui.py
@@ -0,0 +1,13 @@
+# TODO: #24 Create a simple gui
+
+# import tkinter as tk
+
+# window = tk.Tk()
+# button_input_pics_folder = tk.Button(text="Select folder for input pics")
+# button_input_pics_folder.pack
+# entry_input_pics_folder = tk.Entry()
+# button_output_pics_folder = tk.Button(text="Select folder for output pics")
+# button_output_pics_folder.pack
+# button_output_weights_folder = tk.Button(text="Select folder for output weights")
+# button_output_weights_folder.pack
+# window.mainloop()
